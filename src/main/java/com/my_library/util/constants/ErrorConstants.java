c158c58332178775c2c96cda1e8e59e2caa26847
Elena Voronina <elenavoronix@gmail.com>
2026-03-19 11:25:39 +0600

    Add error constants for entities

--- src/main/java/com/my_library/util/constants/ErrorConstants.java
+++ src/main/java/com/my_library/util/constants/ErrorConstants.java
@@ -15,6 +15,15 @@ public final class ErrorConstants {
     public static final String AUTHOR_ID_NULL = "Author ID must not be null for delete";
     public static final String FAILED_TO_ADD = "Adding %s failed, no %s";
     public static final String USER_ID_NULL = "User ID must not be null for delete";
+    public static final String BOOK_COPY_NULL = "Book copy is null";
+    public static final String BOOK_COPY_ID_NULL = "Book copy ID must not be null for delete";
+    public static final String BOOK_NULL = "Book is null";
+    public static final String AUTHOR_NULL = "Author is null";
+    public static final String USER_NULL = "User is null";
+    public static final String LOAN_NULL = "Loan is null";
+    public static final String LOAN_ID_NULL = "Loan ID must not be null for delete";
+    public static final String READER_CARD_NULL = "Reader card is null";
+    public static final String READER_CARD_ID_NULL = "Reader card ID must not be null for delete";
 
     private ErrorConstants() {
     }
